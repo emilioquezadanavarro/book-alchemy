e86d82800d1b2505c42546c0c6405da7dc0199b6
Emilio Quezada <equezadanavarro@gmail.com>
2025-11-21 11:03:20 +0100

    ORM relationship between Books and Authors created

--- data_models.py
+++ data_models.py
@@ -22,7 +22,14 @@ class Book(db.Model):
     isbn = db.Column(db.String)
     title = db.Column(db.String)
     publication_year = db.Column(db.Integer)
+
+    # Foreign Key to link to the authors table
     author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+
+    # The ORM relationship
+    # This creates the 'author' attribute on the Book object, allowing you to use book.author.name
+    # The 'backref' creates the reverse link (author.books)
+    author = db.relationship('Author', backref='books')
 
     def __repr__(self):
         return f"Books(id = {self.id}, name = {self.title})"
